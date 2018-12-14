fb1ee1620233fc7d9631698303a459e6421a1694
5ia16erwalker <5ia16erwalker@gibwintra.ch>
2018-12-14 08:17:55 +0100

    Printed Calculation should also print valueB

--- src/main/java/ch/bbw/ConsoleCalculator/App.java
+++ src/main/java/ch/bbw/ConsoleCalculator/App.java
@@ -18,6 +18,6 @@ public class App
     	
     	valueA = 10;
     	valueB = 20;
-    	System.out.println("Die Summe von " + valueA + " und " + "valueB " + " ist " + calculator.summe(valueA, valueB));
+    	System.out.println("Die Summe von " + valueA + " und " + valueB  + " ist " + calculator.summe(valueA, valueB));
     }
 }
